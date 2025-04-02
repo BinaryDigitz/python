df355e70e95646f2ef6cdebf5608312115377f1e
Binary Digitz <borisndoh@gmail.com>
2025-04-02 17:16:32 +0100

    Array

---
+++ arrays/arrays.py
@@ -0,0 +1,6 @@
+from array import array
+
+list1 = [ 1,2,3,4,5,6,6,7,8,9]
+array1 = array('i', list1)
+
+print(type(array1))
---
+++ tuples/tuples.py
@@ -0,0 +1,39 @@
+
+point1 = (1,2,3)
+
+# no ()
+point2 = 1,2,3,4
+
+# if only one item you should include a last comma (,)
+# point3 = 2,
+
+# tuples can also be added as lists
+
+point4 = point1 + point2
+
+# tuples can also be repeated as lists
+point5 = point1 * 2
+ 
+
+# convert a list to turple
+list1 = [ 1, 2, 3]
+tuple1 = tuple(list1)
+
+
+# tuple are IMMUTABLE
+# tuple cannot be reasigned
+
+# SWAPPING VARIABLES
+
+x = 10
+y = 11
+
+z = x
+x = y
+y = z
+
+#  easeiest way is to defind a turple and unpack it
+
+x, y = y, x
+# still thesame as 
+x, y = ( 11, 10)
