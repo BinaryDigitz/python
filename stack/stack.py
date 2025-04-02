3d9abec4ec42e1e1de457171873dde0f0599d8f3
Binary Digitz <borisndoh@gmail.com>
2025-04-02 16:29:22 +0100

    Example on stacks

---
+++ stack/stack.py
@@ -0,0 +1,10 @@
+# LIFO last in first out.
+
+browsing_session = []
+browsing_session.append(1)
+browsing_session.append(2)
+browsing_session.append(3)
+
+last = browsing_session.pop()
+print(browsing_session)
+print(last)
